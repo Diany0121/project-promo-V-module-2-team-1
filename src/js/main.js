69c9e5b94496ba55793a0f9dcf3e7ee077b2d827
Ángela Ávila Lasanta <lasantavispa@gmail.com>
2023-12-25 11:58:07 +0100

    Java arrow

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+const icon = document.querySelector('.icon');
+const arrowUp = document.querySelector('.fieldset__legend--arrowup');
+const arrowDown = document.querySelector('.fieldset__legend--arrowDown');
+const box = document.querySelector('.boxToBeHidden');
+arrowDown.classList.add('hidden');
+function handleClick() {    
+    box.classList.toggle('hidden'); 
+    arrowUp.classList.toggle('hidden');
+    arrowDown.classList.toggle('hidden');  
+};
+
+icon.addEventListener('click', handleClick);
+
+
