4875ece5821b9fe9caab8786461151b2aa29bff0
Ángela Ávila Lasanta <lasantavispa@gmail.com>
2023-12-26 11:39:59 +0100

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+// ARROW
 const icon = document.querySelector('.icon');
 const arrowUp = document.querySelector('.fieldset__legend--arrowup');
 const arrowDown = document.querySelector('.fieldset__legend--arrowDown');
@@ -8,7 +9,13 @@ function handleClick() {
     arrowUp.classList.toggle('hidden');
     arrowDown.classList.toggle('hidden');  
 };
-
 icon.addEventListener('click', handleClick);
+// RESET
 
+const resetButton = document.querySelector('.');
+function resetForm() {
+    const holeForm = document.querySelector('.holeForm');
+    holeForm.reset();
+};
+resetButton.addEventListener('click', resetForm);
 
