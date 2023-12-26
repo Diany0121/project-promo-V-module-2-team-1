c6c3de18cfc84b8bd7fcbb5375d66fbf52f98b38
Dianelys Martínez del Toro <dianymartinez31@gmai.com>
2023-12-26 11:37:49 +0100

    design

---
+++ src/js/design.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const buttonReset = document.querySelector('.js_reset_btn');
+
+buttonReset.FormElement.reset();
+
+
+const nameUser = document.querySelector('.js-name');
+const jobUser = document.querySelector('.js-job');
+const design = document.querySelector('.js-design');
+
+nameUser.classList.add('paletteOne');
+jobUser.classList.add('paletteOne');
+design.classList.add('paletteOne');
+
