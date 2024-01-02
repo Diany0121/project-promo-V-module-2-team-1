3803532ef0a37fe413f1dab98ab881a6617804da
Dianelys Martínez del Toro <dianymartinez31@gmai.com>
2024-01-02 14:13:47 +0100

    design

--- src/js/design.js
+++ src/js/design.js
@@ -1,3 +1,14 @@
+'use strict';
+
+// const resetButton = document.querySelector('.');
+// function resetForm() {
+//     const holeForm = document.querySelector('.holeForm');
+//     holeForm.reset();
+// };
+// resetButton.addEventListener('click', resetForm);
+
+
+
 // ARROW
 const iconDesign = document.querySelector('.js-iconDesign');
 const arrowUpDesign = document.querySelector('.fieldset__legend--arrowupDesign');
@@ -10,3 +21,20 @@ function handleClickDesign() {
     arrowDownDesign.classList.toggle('hidden');  
 };
 iconDesign.addEventListener('click', handleClickDesign);
+
+
+
+//const paleteOne = document.querySelector('.js-paleteOne');
+//const cardDesign = document.querySelector('.js-design');
+
+
+//function handleClick(){
+    //cardDesign.classList.toggle('paletteOne');
+//};
+//paleteOne.addEventListener('click', handleClick);
+
+//paleteOne.removeEventListener('click', () => {
+    //cardDesign.classList.add("paletteOne");
+//});
+
+
