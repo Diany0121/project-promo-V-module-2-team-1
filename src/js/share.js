6e6432cea7b98e23f0e040c41a33750f463d71b1
gabriela-ppv <gabrielapintovas@gmail.com>
2024-01-04 00:28:25 +0100

    changeorangebutton

--- src/js/share.js
+++ src/js/share.js
@@ -1,29 +1,27 @@
 // ARROW
-const iconShare = document.querySelector('.js-iconShare');
-const arrowUpShare = document.querySelector('.fieldset__legend--arrowupShare');
-const arrowDownShare = document.querySelector('.fieldset__legend--arrowDownShare');
-const boxShare = document.querySelector('.fieldset__boxcontent');
-const buttonShare = document.querySelector('.js-buttonone');
-const createdCard = document.querySelector('.js-createdcard');
+const iconShare = document.querySelector(".js-iconShare");
+const arrowUpShare = document.querySelector(".fieldset__legend--arrowupShare");
+const arrowDownShare = document.querySelector(
+  ".fieldset__legend--arrowDownShare"
+);
+const boxShare = document.querySelector(".fieldset__boxcontent");
+const buttonShare = document.querySelector(".js-buttonone");
+const createdCard = document.querySelector(".js-createdcard");
 
-
-arrowDownShare.classList.add('hidden');
-function handleClickShare() {    
-    boxShare.classList.toggle('hidden'); 
-    arrowUpShare.classList.toggle('hidden');
-    arrowDownShare.classList.toggle('hidden');  
-};
-iconShare.addEventListener('click', handleClickShare);
+arrowDownShare.classList.add("hidden");
+function handleClickShare() {
+  boxShare.classList.toggle("hidden");
+  arrowUpShare.classList.toggle("hidden");
+  arrowDownShare.classList.toggle("hidden");
+}
+iconShare.addEventListener("click", handleClickShare);
 
 // boton naranja/gris y ocualtar seccion
 
-createdCard.classList.add('hidden');
+createdCard.classList.add("hidden");
 
-buttonShare.addEventListener('click', function(event) {
+buttonShare.addEventListener("click", function (event) {
   event.preventDefault();
-  buttonShare.classList.toggle('backgroundorange');
-  buttonShare.classList.toggle('backgroundgrey');
-  createdCard.classList.toggle('hidden');
+  buttonShare.classList.toggle("backgroundgrey");
+  createdCard.classList.toggle("hidden");
 });
-
-
