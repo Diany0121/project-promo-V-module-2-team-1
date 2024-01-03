b17bc93be8a26cd10e3f28845352cdf657b9c16e
gabriela-ppv <gabrielapintovas@gmail.com>
2024-01-03 21:16:28 +0100

    functionbutton

--- src/js/share.js
+++ src/js/share.js
@@ -3,6 +3,10 @@ const iconShare = document.querySelector('.js-iconShare');
 const arrowUpShare = document.querySelector('.fieldset__legend--arrowupShare');
 const arrowDownShare = document.querySelector('.fieldset__legend--arrowDownShare');
 const boxShare = document.querySelector('.fieldset__boxcontent');
+const buttonShare = document.querySelector('.js-buttonone');
+const createdCard = document.querySelector('.js-createdcard');
+
+
 arrowDownShare.classList.add('hidden');
 function handleClickShare() {    
     boxShare.classList.toggle('hidden'); 
@@ -10,3 +14,15 @@ function handleClickShare() {
     arrowDownShare.classList.toggle('hidden');  
 };
 iconShare.addEventListener('click', handleClickShare);
+
+// boton naranja/gris y ocualtar seccion
+
+createdCard.classList.add('hidden');
+
+buttonShare.addEventListener('click', function() {
+  buttonShare.classList.toggle('backgroundorange');
+  buttonShare.classList.toggle('backgroundgrey');
+  createdCard.classList.toggle('hidden');
+});
+
+
