399af52b13369d7ec83ff45b5f1bc55538c9c153
Dianelys Martínez del Toro <dianymartinez31@gmai.com>
2024-01-07 17:43:08 +0100

    design

--- src/js/design.js
+++ src/js/design.js
@@ -2,12 +2,8 @@
 
 // ARROW
 const iconDesign = document.querySelector(".js-iconDesign");
-const arrowUpDesign = document.querySelector(
-  ".fieldset__legend--arrowupDesign"
-);
-const arrowDownDesign = document.querySelector(
-  ".fieldset__legend--arrowDownDesign"
-);
+const arrowUpDesign = document.querySelector(".fieldset__legend--arrowupDesign");
+const arrowDownDesign = document.querySelector(".fieldset__legend--arrowDownDesign");
 const boxDesign = document.querySelector(".form__design--colors");
 arrowDownDesign.classList.add("hidden");
 function handleClickDesign() {
@@ -19,28 +15,33 @@ iconDesign.addEventListener("click", handleClickDesign);
 
 // Variables para los elementos de HTML de la tarjeta
 const resetButton = document.querySelector(".js_reset_btn");
-// const profileName = document.querySelector('.js-name');
-// const profileJob = document.querySelector('.js-job');
+const profileName = document.querySelector('.js-name');
+const profileJob = document.querySelector('.js-job');
 const articleDesign = document.querySelector(".js-designCards");
 const wholeForm = document.querySelector(".wholeForm");
 
 // Variables para los elementos del HTML de la tarjeta, la parte de Diseña
-const palettes = document.querySelector(".js-designPalette");
+const palette = document.querySelector(".js-designPalette");
 const paletteRadiusOne = document.querySelector(".js-paletteOne");
 const paletteRadiusTwo = document.querySelector(".js-paletteTwo");
 const paletteRadiusThree = document.querySelector(".js-paletteThree");
 
 
-function handleClickPallete(event) {
-  articleDesign.classList.remove("paletteOne");
-  articleDesign.classList.remove("paletteTwo");
-  articleDesign.classList.remove("paletteThree");
-  articleDesign.classList.add(`palette${event.currentTarget.value}`);
+function handleClickPaletteOne () {
+  articleDesign.classList.toggle('paletteOne');
 };
 
-paletteRadiusOne.addEventListener('click', handleClickPallete);
-paletteRadiusTwo.addEventListener('click', handleClickPallete);
-paletteRadiusThree.addEventListener('click', handleClickPallete);
+function handleClickPaletteTwo (event){
+  articleDesign.classList.toggle('paletteTwo');
+};
+
+function handleClickPaletteThree () {
+  articleDesign.classList.toggle('paletteThree');
+};
+
+paletteRadiusOne.addEventListener('click', handleClickPaletteOne);
+paletteRadiusTwo.addEventListener('click', handleClickPaletteTwo);
+paletteRadiusThree.addEventListener('click', handleClickPaletteThree);
 
 
 
