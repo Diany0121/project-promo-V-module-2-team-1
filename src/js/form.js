6f3a354a77c83c93fa3ca2ae20125803acda1b33
gabriela-ppv <gabrielapintovas@gmail.com>
2024-01-11 14:07:17 +0100

    changescolapsables

--- src/js/form.js
+++ src/js/form.js
@@ -1,19 +1,4 @@
-// ARROW
-const iconForm = document.querySelector(".js-iconForm");
-const arrowUpForm = document.querySelector(".fieldset__legend--arrowupForm");
-const arrowDownForm = document.querySelector(
-  ".fieldset__legend--arrowDownForm"
-);
-const boxForm = document.querySelector(".boxToBeHidden");
-arrowDownForm.classList.add("hidden");
-function handleClick(box, arrowUp, arrowDown) {
-  box.classList.toggle("hidden");
-  arrowUp.classList.toggle("hidden");
-  arrowDown.classList.toggle("hidden");
-}
-iconForm.addEventListener("click", function () {
-  handleClick(boxForm, arrowUpForm, arrowDownForm);
-});
+
 // INTRODUCIR DATOS
 const nameCard = document.querySelector(".js-name-card");
 const nameForm = document.querySelector(".js-name-form");
--- src/js/share.js
+++ src/js/share.js
@@ -1,23 +1,9 @@
-// ARROW
-const iconShare = document.querySelector(".js-iconShare");
-const arrowUpShare = document.querySelector(".fieldset__legend--arrowupShare");
-const arrowDownShare = document.querySelector(
-  ".fieldset__legend--arrowDownShare"
-);
-const boxShare = document.querySelector(".fieldset__boxcontent");
-const buttonShare = document.querySelector(".js-buttoncreate");
-const createdCard = document.querySelector(".js-createdcard");
 
-arrowDownShare.classList.add("hidden");
-function handleClickShare() {
-  boxShare.classList.toggle("hidden");
-  arrowUpShare.classList.toggle("hidden");
-  arrowDownShare.classList.toggle("hidden");
-}
-iconShare.addEventListener("click", handleClickShare);
 
 //boton naranja/gris y ocualtar seccion
 
+const buttonShare = document.querySelector(".js-buttoncreate");
+const createdCard = document.querySelector(".js-createdcard");
 createdCard.classList.add("hidden");
 
  function handleclickbutton (event) {
