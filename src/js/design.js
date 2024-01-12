43e03546d98ff2afb61ad15f1781decc4cc78e0f
Dianelys Martínez del Toro <dianymartinez31@gmai.com>
2024-01-12 09:13:30 +0100

    design

--- src/js/design.js
+++ src/js/design.js
@@ -5,7 +5,9 @@ const iconDesign = document.querySelector(".js-iconDesign");
 const arrowUpDesign = document.querySelector(".fieldset__legend--arrowupDesign");
 const arrowDownDesign = document.querySelector(".fieldset__legend--arrowDownDesign");
 const boxDesign = document.querySelector(".form__design--colors");
+
 arrowDownDesign.classList.add("hidden");
+
 function handleClickDesign() {
   boxDesign.classList.toggle("hidden");
   arrowUpDesign.classList.toggle("hidden");
