203609684debb5209dccd978873b5ea2c9e1d6bb
Nadine Estrella Vacas <nadinestrella@gmail.com>
2024-01-13 20:44:23 +0100

    containers

--- src/js/colapsables.js
+++ src/js/colapsables.js
@@ -1,17 +1,17 @@
-// ARROW-Desing
+//ARROW ABAJO CERRADO 
+//ARROW ARRIBA ABIERTO
+
+// ARROW-Desing 
 const iconDesign = document.querySelector(".js-fieldset__legend");
-const arrowUpDesign = document.querySelector(
-  ".fieldset__legend--arrowupDesign"
-);
-const arrowDownDesign = document.querySelector(
-  ".fieldset__legend--arrowDownDesign"
-);
+const arrowUpDesign = document.querySelector(".fieldset__legend--arrowupDesign");
+const arrowDownDesign = document.querySelector(".fieldset__legend--arrowDownDesign");
 const boxDesign = document.querySelector(".form__design--colors");
 
 arrowDownDesign.classList.add("hidden");
+
 function clickDesign() {
-  arrowUpDesign.classList.toggle("hidden");
-  arrowDownDesign.classList.toggle("hidden");
+  arrowUpDesign.classList.remove("hidden");
+  arrowDownDesign.classList.add("hidden");
   boxDesign.classList.remove("hidden");
   boxForm.classList.add("hidden");
   boxShare.classList.add("hidden");
@@ -22,17 +22,15 @@ iconDesign.addEventListener("click", collapse);
 // ARROW-form
 const iconForm = document.querySelector(".js-fieldsetLegendForm");
 const arrowUpForm = document.querySelector(".fieldset__legend--arrowupForm");
-const arrowDownForm = document.querySelector(
-  ".fieldset__legend--arrowDownForm"
-);
+const arrowDownForm = document.querySelector(".fieldset__legend--arrowDownForm");
 const boxForm = document.querySelector(".boxToBeHidden");
 
 boxForm.classList.add("hidden");
 arrowUpForm.classList.add("hidden");
 
 function clickForm() {
-  arrowUpForm.classList.toggle("hidden");
-  arrowDownForm.classList.toggle("hidden");
+  arrowUpForm.classList.remove("hidden");
+  arrowDownForm.classList.add("hidden");
   boxDesign.classList.add("hidden");
   boxForm.classList.remove("hidden");
   boxShare.classList.add("hidden");
@@ -43,17 +41,15 @@ iconForm.addEventListener("click", collapse);
 
 const iconShare = document.querySelector(".js-fieldsetLegendShare");
 const arrowUpShare = document.querySelector(".fieldset__legend--arrowupShare");
-const arrowDownShare = document.querySelector(
-  ".fieldset__legend--arrowDownShare"
-);
+const arrowDownShare = document.querySelector(".fieldset__legend--arrowDownShare");
 const boxShare = document.querySelector(".fieldset__boxcontent");
 
 boxShare.classList.add("hidden");
 arrowUpShare.classList.add("hidden");
 
 function clickShare() {
-  arrowUpShare.classList.toggle("hidden");
-  arrowDownShare.classList.toggle("hidden");
+  arrowUpShare.classList.remove("hidden");
+  arrowDownShare.classList.add("hidden");
   boxDesign.classList.add("hidden");
   boxForm.classList.add("hidden");
   boxShare.classList.remove("hidden");
@@ -62,10 +58,10 @@ function clickShare() {
 iconShare.addEventListener("click", collapse);
 
 function collapse(event) {
-  const containerCollapse = event.currentTarget;
+  const containerCollapse = event.currentTarget; //es el contenedor entero a colapsar
   console.log(containerCollapse);
   if (containerCollapse.classList.contains("js-design")) {
-    clickDesign();
+    clickDesign(); //da las instrucciones para las arrow y para q se abran o cierren el resto de contenedores
   } else if (containerCollapse.classList.contains("js-form")) {
     clickForm();
   } else if (containerCollapse.classList.contains("js-share")) {
