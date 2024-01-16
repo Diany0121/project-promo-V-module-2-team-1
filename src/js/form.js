9c359267aa2d6ecc2815b0945a405917017be4af
Nadine Estrella Vacas <nadinestrella@gmail.com>
2024-01-16 13:53:37 +0100

    share

--- src/js/form.js
+++ src/js/form.js
@@ -1,24 +1,24 @@
-
 // INTRODUCIR DATOS
-const nameCard = document.querySelector(".js-name-card");
-const nameForm = document.querySelector(".js-name-form");
-const jobCard = document.querySelector(".js-job-card");
-const jobForm = document.querySelector(".js-job-form");
-const form = document.querySelector(".js-form");
+const nameCard = document.querySelector('.js-name-card');
+const nameForm = document.querySelector('.js-name-form');
+const jobCard = document.querySelector('.js-job-card');
+const jobForm = document.querySelector('.js-job-form');
+const form = document.querySelector('.js-form');
 // const imgCard = document.querySelector('.js-img-card');
 // const imgForm = document.querySelector('.js-img-form');
 
 // ESTO ES PARA TENER LOS DATOS Q METE EL USUARIO EN EL OBJETO QUE LUEGO IRA A LA API
 const dataForm = {
   palette: 1,
-  name: "",
-  job: "",
-  phone: "",
-  email: "",
-  linkedin: "",
-  github: "",
-  photo: "",
+  name: '',
+  job: '',
+  phone: '',
+  email: '',
+  linkedin: '',
+  github: '',
+  photo: '',
 };
+
 // CADA INPUT DEL FORM TIENE QUE TENER UN ID CON EL NOMBRE DE CADA OBJETO
 
 const handleForm = (event) => {
@@ -28,13 +28,13 @@ const handleForm = (event) => {
   console.log(dataForm); //para comprobar que la info entra donde debe
 };
 
-form.addEventListener("input", handleForm);
+form.addEventListener('input', handleForm);
 
 function writeName(event) {
   event.preventDefault();
   const nameFormValue = nameForm.value;
-  if (nameFormValue === "") {
-    nameCard.innerHTML = "Nombre Apellido";
+  if (nameFormValue === '') {
+    nameCard.innerHTML = 'Nombre Apellido';
   } else {
     nameCard.innerHTML = nameFormValue;
   }
@@ -42,13 +42,12 @@ function writeName(event) {
 function writeJob(event) {
   event.preventDefault();
   const jobFormValue = jobForm.value;
-  if (jobFormValue === "") {
-    jobCard.innerHTML = "Front-end developer";
+  if (jobFormValue === '') {
+    jobCard.innerHTML = 'Front-end developer';
   } else {
     jobCard.innerHTML = jobFormValue;
   }
 }
 
-nameForm.addEventListener("input", writeName);
-jobForm.addEventListener("input", writeJob);
-
+nameForm.addEventListener('input', writeName);
+jobForm.addEventListener('input', writeJob);
--- src/js/share.js
+++ src/js/share.js
@@ -1,17 +1,27 @@
-
-
 //boton naranja/gris y ocualtar seccion
 
-const buttonShare = document.querySelector(".js-buttoncreate");
-const createdCard = document.querySelector(".js-createdcard");
-createdCard.classList.add("hidden");
+const buttonShare = document.querySelector('.js-buttoncreate');
+const createdCard = document.querySelector('.js-createdcard');
+createdCard.classList.add('hidden');
 
- function handleclickbutton (event) {
-   event.preventDefault();
-   createdCard.classList.toggle("hidden");
-  buttonShare.classList.toggle("backgroundgrey");
-  
-};
+function handleShare(event) {
+  event.preventDefault();
+  fetch('https://dev.adalab.es/api/card/', {
+    method: 'POST',
+    body: JSON.stringify(dataForm),
+    headers: { 'Content-type': 'application/json' },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
+buttonShare.addEventListener('click', handleShare);
 
-buttonShare.addEventListener("click", handleclickbutton);
+function handleclickbutton(event) {
+  event.preventDefault();
+  createdCard.classList.toggle('hidden');
+  buttonShare.classList.toggle('backgroundgrey');
+}
 
+buttonShare.addEventListener('click', handleclickbutton);
