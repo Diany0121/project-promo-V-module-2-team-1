9ec0056a8fbb301a04c9398a01cbce9e21e72edc
Nadine Estrella Vacas <nadinestrella@gmail.com>
2024-01-22 14:44:32 +0100

    OK SHARE

--- src/js/share.js
+++ src/js/share.js
@@ -1,16 +1,43 @@
 'use strict';
-
-//boton naranja/gris y ocualtar seccion
-
 const buttonShare = document.querySelector('.js-buttoncreate');
 const createdCard = document.querySelector('.js-createdcard');
-const linkCard = document.querySelector('.js-linkCard');
-
 createdCard.classList.add('hidden');
 
 function handleShare(event) {
   event.preventDefault();
+  console.log(dataForm);
+  fetch('https://dev.adalab.es/api/card/', {
+    method: 'POST',
+    body: JSON.stringify(dataForm),
+    headers: { 'Content-type': 'application/json' },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      renderButton();
+    });
+}
+buttonShare.addEventListener('click', handleShare);
 
+function renderButton() {
+  createdCard.classList.toggle('hidden');
+  buttonShare.classList.toggle('backgroundgrey');
+}
+
+// buttonShare.addEventListener('click', handleclickbutton);
+
+/*
+//boton naranja/gris y ocualtar seccion
+
+const createClickCard = document.querySelector('.js-createCard');
+const createdCard = document.querySelector('.js-createdcard');
+const linkCard = document.querySelector('.js-linkCard');
+const twiterBtn = document.querySelector('.js-twiterbtn');
+
+createdCard.classList.add('hidden');
+
+function api(event) {
+  event.preventDefault();
   fetch('https://dev.adalab.es/api/card/', {
     method: 'POST',
     body: JSON.stringify(dataForm), //lo q rellena la usuaria
@@ -19,15 +46,13 @@ function handleShare(event) {
     .then((response) => response.json())
     .then((data) => {
       renderButton(data);
+    })
+    .catch((error) => {
+      console.error('Ups! ha ocurrido un error', error);
     });
-  /*.catch((error) => {
-      console.error('Error:', error);
-      renderButton({ success: false }); // Llama a renderButton con un objeto de éxito falso en caso de error
-    });*/
 }
-buttonShare.addEventListener('click', handleShare);
 
-function renderButton(event) {
+function handleShare(event) {
   if (event.success) {
     // Si el evento es exitoso
     linkCard.href = event.cardURL;
@@ -45,6 +70,8 @@ function renderButton(event) {
   createdCard.classList.toggle('hidden');
   buttonShare.classList.toggle('backgroundgrey');
 }
+
+createClickCard.addEventListener('click', handleShare);
 
 //buttonShare.addEventListener('click', handleclickbutton);
 
