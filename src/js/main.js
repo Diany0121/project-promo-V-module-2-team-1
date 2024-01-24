f3f58d62e62a7086834eae50295edaf27e225f23
Dianelys Martínez del Toro <dianymartinez31@gmai.com>
2024-01-24 20:24:56 +0100

    localstorage

--- src/js/main.js
+++ src/js/main.js
@@ -6,12 +6,3 @@ import './get_avatar.js';
 import './share.js';
 
 
-// RESET
-
-// const resetButton = document.querySelector('.');
-// function resetForm() {
-//     const holeForm = document.querySelector('.holeForm');
-//     holeForm.reset();
-// };
-// resetButton.addEventListener('click', resetForm);
-
